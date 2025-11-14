f52041320fa57184eb409da93efb690b1c178e99
Mina2209 <mina.nassef09@gmail.com>
2025-11-14 19:36:53 +0200

    chore: clean up unused files

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import { FILE_TYPES } from '../constants/fileTypes';
 
